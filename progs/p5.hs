b0587f74723d3e0d98093c7ac21208f56487057a
Andrey Davydov <andrey.a.davydov@gmail.com>
2010-04-06 10:10:52 +0400

    Добавил проверку того, что все удолетворяющие нас простые числа вида 6k+1. Любой желающий может запустить проверку гипотезы и подождать.

--- progs/p5.hs
+++ progs/p5.hs
@@ -12,3 +12,16 @@ res n   = power (n - 1) - (3 * sumc n) - 1
 
 power   :: Integer -> Integer
 power n = product(replicate (fromInteger n) 2) 
+
+findPairs   :: Int -> [(Int, Int)]
+findPairs p = [(a, b) | a <- [1..p-1], b <- [1..p-1], (a * a + a * b + b * b) `mod` p == 0]
+
+pairExists :: Int -> Bool
+pairExists = not . null . findPairs
+
+primes :: [Int]
+primes = filter isPrime [5..] where
+            isPrime     :: Int -> Bool
+            isPrime n   = not (any ((== 0) . (mod n)) [2..floor (sqrt (fromIntegral n))])
+
+hyphotesis = all ((== 1) . (`mod` 6)) (filter pairExists primes)
